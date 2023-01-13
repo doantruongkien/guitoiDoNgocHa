b8960db2e074d936f6397d092bd4344416e2be49
doantruongkien <122629244+doantruongkien@users.noreply.github.com>
2023-01-14 00:26:50 +0700

    Add files via upload
    create card

---
+++ present.js
@@ -0,0 +1,45 @@
+
+var to = '♡';
+var gift_url = 'https://photos.app.goo.gl/6emSHiVEmnsriJ4Y7';
+var gift_image_url = 'https://giadinh.mediacdn.vn/296230595582509056/2022/7/4/x4-1656930969825524267561.jpg';
+
+
+
+var nametag = document.getElementById("nametag");
+var present = document.getElementById("present");
+var presentImage = document.getElementById("present-image");
+
+
+function init() {
+  
+  var _giftLink, 
+      _giftImg;
+  
+  if (gift_url) {
+    _giftLink = document.createElement("a");
+    _giftLink.href = gift_url;
+    _giftLink.target = "_blank";
+    presentImage.appendChild(_giftLink);
+  }
+  
+  if (gift_image_url) {
+    _giftImg = document.createElement("img");
+    _giftImg.src = gift_image_url;
+    if(_giftLink) {
+      _giftLink.appendChild(_giftImg);
+    } else {
+      presentImage.appendChild(_giftImg);
+    }
+  }
+    
+  present.addEventListener("click", function(e){
+    present.classList.toggle("open");
+  }, false);
+  
+  
+  
+  nametag.innerText = to;
+}
+
+init();
+
